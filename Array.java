bc9495616e098126aa0813f946fd4d184499d1e0
divyanshu98163 <divy9anshu>
2024-06-04 09:00:10 +0530

    array in update

--- Array.java
+++ Array.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Array {
     public static void main(String arg[]){
-        int marks[] =new int[50];
-
+           int marks[] =new int[50];
+          System.out.println(marks.length);
         Scanner sc= new Scanner(System.in);
         marks[0] = sc.nextInt();
         marks[1] = sc.nextInt();
@@ -10,5 +10,8 @@ public class Array {
         System.out.println("phy "+marks[0]);
         System.out.println("chem "+marks[1]);
         System.out.println("math "+marks[2]);
+         
+        int   parcentage  = (marks[0]+marks[1]+marks[2])/3;
+        System.out.println("avreage marks"+parcentage);
     }
 }
---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+    public static void update(int marks[]){
+        for(int i = 0; i<marks.length; i++){
+            marks[i] =marks[i]+1;
+        }
+    }
+    public static void main(String[] arg){
+        int marks[] = {89,97,98};
+        update(marks);
+        for(int i =0; i<marks.length; i++){
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+    }
+    
+}
