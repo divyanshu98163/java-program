d644e33e4ab118b518fb90204dd08b8c9b9ba31c
divyanshu98163 <divy9anshu>
2024-06-07 09:38:32 +0530

    Max sub array using prefix array

--- array8.java
+++ array8.java
@@ -2,20 +2,25 @@ public class array8 {
     public static void maxSubarraySum(int number[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        for (int i = 0; i < number.length; i++) {
+        int prefix[] = new int[number.length];
+        prefix[0] = number[0];
+        for(int i =1; i<prefix.length; i++){
+            prefix[i]= prefix[i-1]+number[i];
+
+        }
+        for (int i = 0; i < number.length; i++) 
+        {
+            int start = i;
             for (int j = i; j < number.length; j++) {
-                currSum=0;
-                for (int k = i; k <= j; k++) {
-                    currSum += number[k];
-                }
-                System.out.println(currSum);
+                int end = j;
+                currSum = start ==0 ? prefix[end]: prefix[end]- prefix[start-1];
                 if (maxSum < currSum) {
-                     maxSum =currSum;
+                    maxSum = currSum;
 
                 }
             }
         }
-        System.out.println("max sum "+maxSum);
+        System.out.println("max sum " + maxSum);
     }
 
     public static void main(String arg[]) {
