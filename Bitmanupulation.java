cad3a3046c260b0d99076748371118df5e78faf2
divyanshu98163 <divy9anshu>
2024-06-17 13:34:10 +0530

    binary Bit manupulation

---
+++ Bitmanupulation.java
@@ -0,0 +1,11 @@
+public class Bitmanupulation {
+    public static void main(String[] args) {
+        System.out.println("print binray AND "+(5&6));
+        System.err.println("print binary OR "+(5|6));
+        System.err.println("print binary XOR "+(5^6));
+        System.err.println("print binary one's componenet : "+(~5));
+        System.err.println("print binary left shift "+(5<<2));
+        System.err.println("print binary Right shift "+(6>>1));
+    }
+    
+}
