19982c682d27e5ce33d362ad88357afd917b88e1
divyanshu98163 <divy9anshu>
2024-06-25 14:30:18 +0530

    merge sort

---
+++ DividenConquer.java
@@ -0,0 +1,64 @@
+public class DividenConquer {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.err.println(arr[i]+" ");
+        }
+        System.err.println();
+    }
+
+
+    public static void mergeSort(int arr[],int si ,int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int mid = si+(ei-si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si,mid, ei);
+    }
+
+    public static void merge(int arr[],int si,int mid,int ei){
+        int temp[] = new int[ei-si+1];
+        int i = si; // iterator for left part
+        int j = mid +1; // iterator for right part 
+        int k =0; // iterator for temp arr 
+
+
+
+        while(i<=mid && j<=ei){
+            if(arr[i] <arr[j]){
+                temp[k] =arr[i];
+                i++;
+            }
+            else{
+                temp[k]= arr[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        // right temp to original arr
+        while(i<=mid){
+            temp[k++] =arr[i++];
+        }
+        // left temp to original arr
+        while(j<=ei){
+            temp[k++] =arr[j++];
+        }
+
+        // copy temp to original arr 
+        for(k =0,i =si; k<temp.length;k++,i++){
+            arr[i] =temp[k];
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,5,2,8};
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+        
+    }
+    
+}
